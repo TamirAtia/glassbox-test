5e9eec9034888027c461e30f70459def278a2275
Tamir Atia <tamir111994@gmail.com>
2022-09-21 18:05:48 +0300

    working project, need some cleaning the code and add redirect after add employee

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,59 +1,49 @@
-const EmployeeModel = require('../models/employee.model');
+const EmployeeModel = require("../models/employee.model");
 
-const HttpException = require('../utils/HttpException.utils');
-const { validationResult } = require('express-validator');
-
-
+const HttpException = require("../utils/HttpException.utils");
+const { validationResult } = require("express-validator");
 
 class EmployeeController {
+  getEmployees = async (req, res) => {
+    await EmployeeModel.findAll().then(data => {
+        console.log(res)
+        res.send(data);
+        
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+  };
 
-getEmployees = async (req, res) => {
+  addEmployee = async (req, res, next) => {
     this.checkValidation(req);
-
-    
-    let employees = await EmployeeModel.findAndCountAll()
-
-    if (!employees) {
-        throw new HttpException(404, 'Unable to get employees!');
+    const { id, name, department, role } = req.body;
+    console.log(id, name, department, role);
+    if (!id || !name || !department || !role) {
+      throw new HttpException(401, "Missing Parameters");
     }
-    if (employees){
-    // Found Employees
-    employees = employees.dataValues
-
-    
-    res.send({    employees  });   
-}
-};
-addEmployee = async (req, res, next) => {
-    this.checkValidation(req);
-    const { id,name, department, role } = req.body;
-    console.log(id,name, department, role)
-    if (!id || !name || !department || !role){
-        throw new HttpException(401, 'Missing Parameters')
-    }
-    await EmployeeModel.create({ id,name, department, role })
-    .then((result)=>{
-        console.log(result)
-        const {id,name, department, role} = result.dataValues 
-        res.send({id,name, department, role})
-    }).catch((err) => {
-        if(err.errors){
-            throw new HttpException(401, 'Unable to add employee!', err.errors);
-
+    await EmployeeModel.create({ id, name, department, role })
+      .then((result) => {
+        console.log(result);
+        const { id, name, department, role } = result.dataValues;
+        res.send({ id, name, department, role });
+      })
+      .catch((err) => {
+        if (err.errors) {
+          throw new HttpException(401, "Unable to add employee!", err.errors);
         }
-        throw new HttpException(401, 'Unexpected Eroor happend!', err);
-    })
+        throw new HttpException(401, "Unexpected Eroor happend!", err);
+      });
+  };
 
-}
-
-
-checkValidation = (req) => {
-    const errors = validationResult(req)
+  checkValidation = (req) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        throw new HttpException(400, 'Validation failed', errors);
+      throw new HttpException(400, "Validation failed", errors);
     }
+  };
 }
-
-
-}
-module.exports = new EmployeeController;
+module.exports = new EmployeeController();
--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 const EmployeeRouter = express.Router();
 const EmployeeController = require('../controllers/employee.controller'); //login by username and password
 
-const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
+// const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
 
 
 
-EmployeeRouter.post('/addEmployee', awaitHandlerFactory(EmployeeController.addEmployee)) 
-EmployeeRouter.get('/getEmployees', awaitHandlerFactory(EmployeeController.getEmployees))
+EmployeeRouter.post('/add', EmployeeController.addEmployee)
+EmployeeRouter.get('/getEmployees', EmployeeController.getEmployees)
 
 module.exports = EmployeeRouter;
